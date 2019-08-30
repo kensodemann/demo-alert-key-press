28175306f35b10381071f388316990f021d45031
Ken Sodemann <ken@ionic.io>
2019-08-30 13:35:46 -0500

    add alert

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  constructor(private alertController: AlertController) {}
 
-  constructor() {}
+  async getData() {
+    const alert = await this.alertController.create({
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Placeholder 1'
+        },
+        {
+          name: 'name6',
+          type: 'number'
+        },
+        {
+          name: 'name7',
+          type: 'number'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Ok',
+          role: 'doit'
+        }
+      ]
+    });
 
+    await alert.present();
+    alert.onkeyup = ev => console.log(ev.keyCode);
+  }
 }
