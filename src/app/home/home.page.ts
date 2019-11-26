eb766d72ae0a890e1bc27ae720cd0a754a7e1bdf
Ken Sodemann <ken@ionic.io>
2019-11-26 13:42:42 -0600

    add different sized alerts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,6 +9,28 @@ import { AlertController } from '@ionic/angular';
 export class HomePage {
   constructor(private alertController: AlertController) {}
 
+  async showBigAlert() {
+    const alert = await this.alertController.create({
+      header: 'This is Bob',
+      message:
+        'My name is Bob. I am very loquacious, which means that I talk. A lot. This also means that I have a tendency to drive Ken nuts. ' +
+        'You see, Ken likes people who do not talk much. His ideal situation would be to date a librarian and then go to a library ' +
+        'where they could both just sit and read and not talk to one another. In the eyes of Ken, this would be an ideal date. ' +
+        'Ken is a very weird guy and this is why he finds me very anoying.',
+      buttons: ['Disagree', 'Agree']
+    });
+    alert.present();
+  }
+
+  async showSmallAlert() {
+    const alert = await this.alertController.create({
+      header: 'This is Ken',
+      message: 'Hey Bob, please shut up.',
+      buttons: ['Disagree', 'Agree']
+    });
+    alert.present();
+  }
+
   async getData() {
     const alert = await this.alertController.create({
       header: 'Prompt!',
